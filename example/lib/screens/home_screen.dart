eae5015a43c6e95d3428d093acec72542579fbc4
Volodymyr Sydorko <v.sydorko@softonix.org>
2023-12-20 17:23:18 +0200

    Refactor

--- example/lib/screens/home_screen.dart
+++ example/lib/screens/home_screen.dart
@@ -3,7 +3,7 @@ import 'package:example/screens/notes/notes_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
